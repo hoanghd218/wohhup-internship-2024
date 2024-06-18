d3fc543e409478103ac7453316e6c1ad04a3ffff
TH2HieuNguyenTrung0094166 <anreo.me+github@gmail.com>
2024-06-18 17:39:06 +0700

    update react

---
+++ REACT/2024-06-18/App.js
@@ -0,0 +1,11 @@
+function App() {
+   return (
+      <div id="wrapper">
+         <Header
+            image="."
+            title="VueJS" 
+            desc="hmmm"
+         />
+      </div>
+   )
+}
